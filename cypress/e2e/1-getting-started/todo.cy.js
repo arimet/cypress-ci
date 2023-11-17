3afde7457723e6f8856b3e34d88f7df4da4fadb3
Anthony Rimet <anthony.rimet03@gmail.com>
2023-11-17 10:31:28 +0100

    Feat(config): Init cypress

---
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -0,0 +1,12 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/todo')
+  })
+
+  it('displays two todo items by default', () => {
+    cy.get('.todo-list li').should('have.length', 2)
+    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    cy.get('h1').should('have.text', 'TEST FAILED');
+  })
+})
